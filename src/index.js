ba40042a4949a884d82d1f753320105503787aa3
umairnaeem373 <umairnaeem373@gmail.com>
2024-05-01 17:25:40 +0500

    some fixes

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter , HashRouter } from 'react-router-dom';
 import { AppProvider } from './data/store/createContext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter basename='/'>
   <AppProvider>
     <App />
   </AppProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
